165f8b8df9a6a9b4232420ddba7f04789929d41d
Lux <fails9503@gmail.com>
2022-06-14 15:11:48 +0900

    Terraform_Cloud  - env Test 4

--- Provider.tf
+++ Provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "openstack" {
   user_name   = "cloudmsp@wins21.co.kr"
   tenant_id   = "649bbc87313648de8d55b9ed50ca4b3e"
-  password    = "$password"
+  password    = var.nhn_password
   auth_url    = "https://api-identity.infrastructure.cloud.toast.com/v2.0"
   region      = "KR1"
 }
