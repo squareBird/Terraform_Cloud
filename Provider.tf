61c918c7aa4d591e85763923ce29b153b49400bb
Lux <fails9503@gmail.com>
2022-06-14 14:36:48 +0900

    Terraform_Cloud  - required_providers 정의

--- Provider.tf
+++ Provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
 provider "openstack" {
   user_name   = "cloudmsp@wins21.co.kr"
   tenant_id   = "649bbc87313648de8d55b9ed50ca4b3e"
