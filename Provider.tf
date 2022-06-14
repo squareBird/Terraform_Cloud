c15e962e52aba8c78d65324ff637c583235d2c4d
Lux <fails9503@gmail.com>
2022-06-14 14:23:02 +0900

    Terraform_Cloud  - provider 정의

---
+++ Provider.tf
@@ -0,0 +1,7 @@
+provider "openstack" {
+  user_name   = "cloudmsp@wins21.co.kr"
+  tenant_id   = "649bbc87313648de8d55b9ed50ca4b3e"
+  password    = "sp123$here "
+  auth_url    = "https://api-identity.infrastructure.cloud.toast.com/v2.0"
+  region      = "KR1"
+}
